4917b28cebbe051cb8c73b036fe1ccb35b4b7b71
shen-0806 <houshen.0806@gmail.com>
2026-06-15 10:24:04 +0800

    Update MUB Links

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function loadLinks() {
             {"id":4,"name":"K A/AL Fan","url":"https://www.systemair.com/en-my/products/fans/duct-fans/circular-duct-fans"},
             {"id":5,"name":"CDRE/D","url":"https://www.systemair.com/en-my/products/fans/duct-fans/square-duct-fans/cdr"},
             {"id":6,"name":"MUB T","url":"https://www.systemair.com/en-my/products/fans/duct-fans/insulated-duct-fans/mub/mub-t"},
-            {"id":7,"name":"MUB","url":"https://www.systemair.com/en-my/products/fans/duct-fans/insulated-duct-fans/mub/mub#tech-specs-features"},
+            {"id":7,"name":"MUB","url":"https://www.systemair.com/en-my/products/fans/duct-fans/insulated-duct-fans/mub/mub"},
             {"id":8,"name":"BKF Duct Fan","url":"https://www.systemair.com/en-my/products/fans/duct-fans/rectangular-duct-fans/bkf"},
             {"id":9,"name":"ASP Centrifugal","url":"https://www.systemair.com/en-my/products/selection-software/centrifugal-fans?ecom-category-id-MULTI=90101"}
         ]; 
